73b1204dfba5e746181a8d4ef8f15346546f50df
YuanchenZhu2020 <yuanchenzhu@126.com>
2021-08-21 16:09:47 +0800

    finished polt utils

--- R/plot_utils.R
+++ R/plot_utils.R
@@ -54,7 +54,6 @@ bar_line_ggplot <- function(
 ) {
   # use first column as x axis variable if `x_var` is not giving
   x_var <- ifelse(!missing(x_var), rlang::quo_name(x_var), colnames(dataset)[1])
-  bar_var <- "sample_size"
   line_var <- base::setdiff(colnames(dataset), c(x_var, bar_var))[1]
 
   # get sacle params and funcs for secondary axis
@@ -186,13 +185,13 @@ bar_group_echart <- function(
     ))
   }
 
-  # find the bound of secondary Y axis.
-  last_index <- length(bar_var)
+  # find the bound of main Y axis.
+  first_bar_bounds <- get_bounds(data, bar_var[1])
   main_lower_bound <- ifelse(
-    !missing(main_lower_bound), main_lower_bound, get_odds_bounds(data, bar_var[1])
+    !missing(main_lower_bound), main_lower_bound, first_bar_bounds[1]
   )
   main_upper_bound <- ifelse(
-    !missing(main_upper_bound), main_upper_bound, get_odds_bounds(data, bar_var[1], lower = FALSE)
+    !missing(main_upper_bound), main_upper_bound, first_bar_bounds[2]
   )
 
   if (!is.null(timeline_var)) e <- data |> dplyr::group_by(.data[[timeline_var]])
@@ -202,6 +201,7 @@ bar_group_echart <- function(
     echarts4r::e_charts_(x_var, timeline = !is.null(timeline_var))
   e <- e |>
     echarts4r::e_bar_(bar_var[1], name = bar_name[1], emphasis = list(focus = emphasis_type[1]))
+  last_index <- length(bar_var)
   for (i in 2:last_index) {
     e <- e |>
       echarts4r::e_bar_(
@@ -299,8 +299,7 @@ heatmap_echart <- function(
   )
 
   # scale function
-  value_upper_bound <- get_odds_bounds(data, value_var, lower = FALSE)
-  if (max(data[[value_var]]) < value_upper_bound / 2) value_upper_bound <- max(data[[value_var]])
+  value_upper_bound <- get_bounds(data, value_var, upper_shrink = TRUE)[2]
   visual_scale <- function(x) {
     if (!inherits(x, "numeric")) {
       stop("x must be numeric")
@@ -390,7 +389,7 @@ map_echart <- function(
   }
 
   # get upper bound of visual map
-  rate_upper <- get_odds_bounds(data, value_var, lower = FALSE)
+  rate_upper <- get_bounds(data, value_var)[2]
   visual_scale_func <- function(x){
     if (!inherits(x, "numeric")) {
       stop("x must be numeric")
@@ -440,5 +439,328 @@ map_echart <- function(
         padding = 0
       )
   }
+  return(e)
+}
+
+#' Bar and Line Chart (with Timeline)
+#'
+#' @description Plot bar and line chart using \code{\link[echarts4r]{e_bar}} and
+#' \code{\link[echarts4r]{e_line}}. Regardless of whether there is a timeline axis,
+#' this function can plot charts correctly.
+#'
+#' @param data a data frame
+#' @param timeline_var character. The variable indicating timeline.
+#' @param x_var character. The variable that will be the X axis in the chart.
+#' @param bar_var character vector. The variable that will be used to draw bar chart.
+#' @param line_var character vector. The variable that will be used to draw line chart.
+#' @param main_axis_name character. The name of main Y axis. If missing, the function will use
+#'   information in \code{option} to auto-detect the name according to \code{bar_var}.
+#' @param sec_axis_name character. The name of secondary Y axis. If missing, the function will use
+#'   information in \code{option} to auto-detect the name according to \code{line_var}.
+#' @param x_axis_name character. The name of X axis. If missing, the function will use
+#'   information in \code{option} to auto-detect the name according to \code{x_var}.
+#' @param axis_fontsize numeric, default 12. The font size of axis name.
+#' @param x_name_gap numeric, default 25. The distance between x axis and label of x axis.
+#' @param long_x_label logical, default FALSE. Whether to use style that solving the problem than
+#'   the label text of x axis is too long.
+#' @param x_label_width numeric, default 30. The label text width of x axis.
+#' @param use_y_upper_bound logical, default FALSE. Whether to use upper bound of Y axis.
+#' @param show_label logical, default TRUE. Whether to show data labels.
+#' @param toolbox_features character vectors, default \code{c('saveAsImage', 'restore', 'dataView')}.
+#'   It controls the toolbox functions in echarts.
+#'
+#' @return an object of class \code{echarts4r}.
+#' @export
+bar_line_echart <- function(
+  data, timeline_var = NULL, x_var, bar_var, line_var,
+  main_axis_name, sec_axis_name, x_axis_name,
+  axis_fontsize = 12, x_name_gap = 25, long_x_label = FALSE, x_label_width = 30,
+  use_y_upper_bound = FALSE, show_label = TRUE,
+  toolbox_features = c('saveAsImage', 'restore', 'dataView')
+) {
+  # use first column as x axis variable if `x_var` is not giving
+  x_var <- ifelse(!missing(x_var), rlang::quo_name(x_var), colnames(data)[1])
+  bar_var <- ifelse(!missing(bar_var), bar_var, "sample_size")
+  line_var <- ifelse(
+    !missing(line_var), line_var, setdiff(colnames(data), c(timeline_var, x_var, bar_var))[1]
+  )
+
+  # auto-detect column names
+  colnames_map <- getOption("report.options")[["colnameMap"]]
+  if (is.null(colnames_map)) {
+    message("You donnot set the `report.options$colnameMap` in `options()` before using name auto-detection.")
+    message("We will use given params or column names of dataset to auto-detect axis name in figure.")
+    colnames_map <- list()
+    colnames_map[[bar_var]] = bar_var
+    colnames_map[[line_var]] = line_var
+    colnames_map[[x_var]] = x_var
+  }
+  if (missing(main_axis_name)) {
+    main_axis_name <- ifelse(
+      !is.null(colnames_map[[bar_var]]), colnames_map[[bar_var]], bar_var
+    )
+  }
+  if (missing(sec_axis_name)) {
+    sec_axis_name <- ifelse(
+      !is.null(colnames_map[[line_var]]), colnames_map[[line_var]], line_var
+    )
+  }
+  if (missing(x_axis_name)) {
+    x_axis_name <- ifelse(
+      !is.null(colnames_map[[x_var]]), colnames_map[[x_var]], x_var
+    )
+  }
+
+  # find the lower bound of secondary Y axis.
+  sec_y_bounds <- get_bounds(data, line_var, upper_shrink = TRUE)
+  if (!use_y_upper_bound) sec_y_bounds[2] <- 100
+
+  # set the x axis label style
+  if (long_x_label) x_label_style <- list(interval = 0, width = x_label_width, overflow = "break")
+  else x_label_style <- list(interval = 0)
+
+  # group data if using timeline_var
+  if (!is.null(timeline_var)) e <- data |> dplyr::group_by(.data[[timeline_var]])
+  else e <- data
+
+  e <- e |>
+    echarts4r::e_charts_(x_var, timeline = !is.null(timeline_var)) |>
+    echarts4r::e_line_(line_var, name = sec_axis_name, y_index = 0) |>
+    echarts4r::e_bar_(bar_var, name = main_axis_name, y_index = 1) |>
+    echarts4r::e_labels(show = show_label, fontWeight = "bold") |>
+    echarts4r::e_tooltip(trigger = "axis") |>
+    echarts4r::e_toolbox_feature(feature = toolbox_features) |>
+    echarts4r::e_x_axis_(
+      name = x_axis_name, nameLocation = "middle", nameGap = x_name_gap,
+      nameTextStyle = list(fontSize = axis_fontsize),
+      axisLabel = x_label_style
+    ) |>
+    echarts4r::e_y_axis_(
+      index = 0, name = sec_axis_name, nameGap = 10,
+      nameTextStyle = list(fontSize = axis_fontsize),
+      min = sec_y_bounds[1], max = sec_y_bounds[2]
+    )
+  if (is.null(timeline_var)) {
+    e <- e |>
+      echarts4r::e_y_axis_(
+        index = 1, name = main_axis_name, nameGap = 10,
+        nameTextStyle = list(fontSize = axis_fontsize)
+      )
+  }
+  return(e)
+}
+
+#' Scatter Chart
+#'
+#' @param data a data frame
+#' @param x_var character. The variable that will be the X axis in the chart.
+#' @param y_var character. The variable that will be the Y axis in the chart.
+#' @param label_var character. The variable that will be the label of point in the chart.
+#' @param x_axis_name character. The name of X axis. If missing, the function will use
+#'   information in \code{option} to auto-detect the name according to \code{x_var}.
+#' @param y_axis_name character. The name of Y axis. If missing, the function will use
+#'   information in \code{option} to auto-detect the name according to \code{y_var}.
+#' @param x_lower_bound numeric. The lower bound of x axis.
+#' @param x_upper_bound numeric. The upper bound of x axis.
+#' @param y_lower_bound numeric. The lower bound of Y axis.
+#' @param y_upper_bound numeric. The upper bound of x axis.
+#' @param increment numeric, default 1000. The increment of secondary Y axis ticks.
+#' @param symbol_size numeric, default 20. The size of point in chart.
+#' @param shrink_width character, default NULL. It uses \code{\link[echarts4r]{e_grid}} to
+#'   control the width of figure grid so the long name of X axis can be seen fully.
+#' @param toolbox_features character vectors, default \code{c("saveAsImage", "restore")}. It controls
+#'   the toolbox functions in echarts.
+#'
+#' @return an object of class \code{echarts4r}.
+#' @export
+scatter_echart <- function(
+  data, x_var, y_var, label_var,
+  x_axis_name, y_axis_name,
+  x_lower_bound, x_upper_bound, y_lower_bound, y_upper_bound,
+  increment = 1000, symbol_size = 20, shrink_width = NULL,
+  toolbox_features = c('saveAsImage', 'restore')
+) {
+  # auto-detect column names
+  colnames_map <- getOption("report.options")[["colnameMap"]]
+  if (is.null(colnames_map)) {
+    message("You donnot set the `report.options$colnameMap` in `options()` before using name auto-detection.")
+    message("We will use given params or column names of dataset to auto-detect axis name in figure.")
+    colnames_map <- list()
+    colnames_map[[x_var]] = x_var
+    colnames_map[[y_var]] = y_var
+  }
+  if (missing(x_axis_name)) {
+    x_axis_name <- ifelse(
+      !is.null(colnames_map[[x_var]]), colnames_map[[x_var]], x_var
+    )
+  }
+  if (missing(y_axis_name)) {
+    y_axis_name <- ifelse(
+      !is.null(colnames_map[[y_var]]), colnames_map[[y_var]], y_var
+    )
+  }
+
+  # find the lower bound of secondary Y axis.
+  x_bounds <- get_bounds(data, x_var, increment = increment, upper_shrink = TRUE)
+  x_lower_bound <- ifelse(
+    !missing(x_lower_bound), x_lower_bound, x_bounds[1]
+  )
+  x_upper_bound <- ifelse(
+    !missing(x_upper_bound), x_upper_bound, x_bounds[2]
+  )
+  y_bounds <- get_bounds(data, x_var, upper_shrink = TRUE)
+  y_lower_bound <- ifelse(
+    !missing(y_lower_bound), y_lower_bound, y_bounds[1]
+  )
+  y_upper_bound <- ifelse(
+    !missing(y_upper_bound), y_upper_bound, y_bounds[2]
+  )
+
+  e <- data |>
+    echarts4r::e_charts_(x_var) |>
+    echarts4r::e_scatter_(
+      y_var, label_var,
+      name = y_axis_name,
+      label = list(
+        show = TRUE,
+        formatter = htmlwidgets::JS("function(params){return(params.value[2])}"),
+        position = "right",
+        minMargin = 2
+      ),
+      labelLine = list(
+        show = TRUE,
+        length2 = 5,
+        distance = 10,
+        lineStyle = list(color = "#bbb")
+      ),
+      symbolSize = symbol_size,
+      emphasis = list(focus = "self")
+    ) |>
+    echarts4r::e_y_axis(
+      name = y_axis_name,
+      min = y_lower_bound, max = y_upper_bound,
+      splitLine = list(show = FALSE)
+    ) |>
+    echarts4r::e_x_axis(
+      name = x_axis_name,
+      min = x_lower_bound, max = x_upper_bound,
+      splitLine = list(show = FALSE)
+    ) |>
+    echarts4r::e_tooltip(trigger = "item") |>
+    echarts4r::e_toolbox_feature(feature = toolbox_features) |>
+    echarts4r::e_toolbox_feature(c("dataZoom")) |>
+    echarts4r::e_datazoom(x_index = 0, type = "inside") |>
+    echarts4r::e_datazoom(y_index = 0, type = "inside")
+  if (!is.null(shrink_width)) {
+    e <- e |> echarts4r::e_grid(width = shrink_width)
+  }
+  return(e)
+}
+
+#' Scatter Chart with Timeline
+#'
+#' @param data a data frame
+#' @param timeline_var character. The variable indicating timeline.
+#' @param x_var character. The variable that will be the X axis in the chart.
+#' @param y_var character. The variable that will be the Y axis in the chart.
+#' @param size_var character. The variable that will be the label of point in the chart.
+#' @param x_axis_name character. The name of X axis. If missing, the function will use
+#'   information in \code{option} to auto-detect the name according to \code{x_var}.
+#' @param y_axis_name character. The name of Y axis. If missing, the function will use
+#'   information in \code{option} to auto-detect the name according to \code{y_var}.
+#' @param size_axis_name character. The name of size axis. If missing, the function will use
+#'   information in \code{option} to auto-detect the name according to \code{size_var}.
+#' @param y_lower_bound numeric. The lower bound of Y axis.
+#' @param y_upper_bound numeric. The upper bound of Y axis.
+#' @param x_label_width numeric, default 50. The width of label text of x axis.
+#' @param symbol_size numeric vector, default \code{c(1,25)}. The range of size of point in chart.
+#' @param timeline_top numeric, default 5. The distance between timeline widget and top of chart.
+#' @param interval numeric, default 0. The interval of label of timeline points.
+#' @param timeline_label_width numeric, default 50. The label text width of timeline points.
+#' @param toolbox_features character vectors, default \code{c("saveAsImage", "restore")}. It controls
+#'   the toolbox functions in echarts.
+#'
+#' @return an object of class \code{echarts4r}.
+#' @export
+scatter_timeline_echart <- function(
+  data, timeline_var, x_var, y_var, size_var,
+  x_axis_name, y_axis_name, size_axis_name,
+  y_lower_bound, y_upper_bound, x_label_width = 50, symbol_size = c(1, 25),
+  timeline_top = 5, interval = 0, timeline_label_width = 50,
+  toolbox_features = c('saveAsImage', 'restore')
+) {
+  # auto-detect column names
+  colnames_map <- getOption("report.options")[["colnameMap"]]
+  if (is.null(colnames_map)) {
+    message("You donnot set the `report.options$colnameMap` in `options()` before using name auto-detection.")
+    message("We will use given params or column names of dataset to auto-detect axis name in figure.")
+    colnames_map <- list()
+    colnames_map[[x_var]] = x_var
+    colnames_map[[y_var]] = y_var
+    colnames_map[[size_var]] = size_var
+  }
+  if (missing(x_axis_name)) {
+    x_axis_name <- ifelse(
+      !is.null(colnames_map[[x_var]]), colnames_map[[x_var]], x_var
+    )
+  }
+  if (missing(y_axis_name)) {
+    y_axis_name <- ifelse(
+      !is.null(colnames_map[[y_var]]), colnames_map[[y_var]], y_var
+    )
+  }
+  if (missing(size_axis_name)) {
+    size_axis_name <- ifelse(
+      !is.null(colnames_map[[size_var]]), colnames_map[[size_var]], size_var
+    )
+  }
+
+  # find the lower bound of secondary Y axis.
+  y_bounds <- get_bounds(data, y_var)
+  y_lower_bound <- ifelse(!missing(y_lower_bound), y_lower_bound, y_boundsp[1])
+  y_upper_bound <- ifelse(!missing(y_upper_bound), y_upper_bound, y_bounds[2])
+
+  # formatter str
+  formatter_str <- sprintf(
+    "function(params){return('<strong>' + params.value[0]
+    + '</strong><br/>%s: ' + params.value[1]  + '<br/>%s: ' + params.value[2])}",
+    y_axis_name, size_axis_name
+  )
+
+  e <- data |>
+    dplyr::group_by(.data[[timeline_var]]) |>
+    echarts4r::e_charts_(x_var, timeline = TRUE) |>
+    echarts4r::e_scatter_(y_var, size_var, name = y_axis_name, emphasis = list(focus = "self")) |>
+    echarts4r::e_legend(show = FALSE) |>
+    echarts4r::e_x_axis(
+      name = x_axis_name,
+      axisLabel = list(interval = 0, width = x_label_width, overflow = "break"),
+      splitLine = list(show = TRUE)
+    ) |>
+    echarts4r::e_y_axis(name = y_axis_name, min = y_lower_bound, max = y_upper_bound) |>
+    echarts4r::e_visual_map(
+      left = "right", top = "25%", dimension = 2,
+      text = list(paste0("\u5706\u5f62\u5927\u5c0f\uff1a\n", size_axis_name)),
+      textGap = 30,
+      min = 0, max = max(data[[size_var]]),
+      calculable = TRUE,
+      inRange = list(symbolSize = symbol_size)
+    ) |>
+    echarts4r::e_tooltip(
+      trigger = "item",
+      formatter = htmlwidgets::JS(formatter_str)
+    ) |>
+    echarts4r::e_timeline_opts(
+      playInterval = 1500, top = timeline_top,
+      label = list(
+        position = "bottom", interval = interval,
+        width = timeline_label_width, overflow = "break"
+      ),
+      padding = 0
+    ) |>
+    echarts4r::e_toolbox_feature(feature = toolbox_features) |>
+    echarts4r::e_toolbox_feature(c("dataZoom")) |>
+    echarts4r::e_datazoom(x_index = 0, type = "inside") |>
+    echarts4r::e_datazoom(y_index = 0, type = "inside")
   return(e)
 }
